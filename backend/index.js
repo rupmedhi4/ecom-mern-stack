3fc50f978a3435ac165e4b57505ebf1e4410b780
Rup medhi <rupmmedhi@gmail.com>
2025-07-01 13:30:20 +0530

    add url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://ecommerce-app-frontend-flame-seven.vercel.app",
     credentials: true,
 }));
 
